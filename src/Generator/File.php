9c2f10b1e3645fe1117bb8b7e9ef7bcb4088d6a6
n.hvorostenko@civenty.com <hvorostenko.nikita@gmail.com>
2021-01-28 11:48:59 +0200

    file generator UPDATE (extension parameter added)

--- src/Generator/File.php
+++ src/Generator/File.php
@@ -5,8 +5,10 @@ namespace HNV\Http\Helper\Generator;
 
 use LogicException;
 
+use function strlen;
 use function is_file;
 use function tempnam;
+use function rename;
 use function unlink;
 use function sys_get_temp_dir;
 /** ***********************************************************************************************
@@ -18,11 +20,37 @@ use function sys_get_temp_dir;
 class File extends AbstractGenerator implements GeneratorInterface
 {
     /** **********************************************************************
+     * Constructor.
+     *
+     * @param string $extension             File extension, optional.
+     ************************************************************************/
+    public function __construct(private string $extension = '') {}
+    /** **********************************************************************
      * @inheritDoc
      *
      * @return string                       Generated temporary file path.
      ************************************************************************/
     public function generate(): string
+    {
+        $filePath           = $this->createTemporaryFile();
+        $filePathComplete   = strlen($this->extension) > 0
+            ? $this->addTemporaryFileExtension($filePath, $this->extension)
+            : $filePath;
+
+        $this->clear(function() use ($filePathComplete) {
+            if (is_file($filePathComplete)) {
+                unlink($filePathComplete);
+            }
+        });
+
+        return $filePathComplete;
+    }
+    /** **********************************************************************
+     * Create temporary file and get its path.
+     *
+     * @return string                       File path.
+     ************************************************************************/
+    private function createTemporaryFile(): string
     {
         $temporaryDirectory = sys_get_temp_dir();
         $temporaryFile      = tempnam($temporaryDirectory, 'UT');
@@ -31,12 +59,25 @@ class File extends AbstractGenerator implements GeneratorInterface
             throw new LogicException('temporary file creating failed');
         }
 
-        $this->clear(function() use ($temporaryFile) {
-            if (is_file($temporaryFile)) {
-                unlink($temporaryFile);
-            }
-        });
-
         return $temporaryFile;
+    }
+    /** **********************************************************************
+     * Add temporary file extension and get its new complete path.
+     *
+     * @param   string  $filePath           File path.
+     * @param   string  $extension          Need file extension.
+     *
+     * @return  string                      File path new.
+     ************************************************************************/
+    private function addTemporaryFileExtension(string $filePath, string $extension): string
+    {
+        $filePathWithExtension  = "$filePath.$extension";
+        $renameSuccess          = rename($filePath, $filePathWithExtension);
+
+        if ($renameSuccess === false) {
+            throw new LogicException('file renaming failed');
+        }
+
+        return $filePathWithExtension;
     }
 }
